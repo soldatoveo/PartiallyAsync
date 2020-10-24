f8418853c4620d622a4cae7a8fe093417c4ceffd
Евгений Солдатов <soldatov.eo@gmail.com>
2020-10-24 13:52:20 +0700

    Add project files.

---
+++ PartiallyAsync/AsyncCaller.cs
@@ -0,0 +1,27 @@
+﻿namespace PartiallyAsync
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AsyncCaller
+    {
+        private readonly EventHandler eventHandler;
+
+        public AsyncCaller(EventHandler eventHandler)
+        {
+            this.eventHandler = eventHandler;
+        }
+
+        public bool Invoke(int timeToWaitBeforeReturn, object sender, EventArgs args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = new Task(() => this.eventHandler.Invoke(sender, args), token);
+            task.Start();
+            var completed = task.Wait(timeToWaitBeforeReturn, token);
+
+            return completed;
+        }
+    }
+}
---
+++ PartiallyAsync/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PartiallyAsync
+{
+    using System;
+    using System.Threading;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ac = new AsyncCaller(Wr);
+            var completedOk = ac.Invoke(2000, null, EventArgs.Empty);
+            var text = $"Задание выполнено вовремя: {(completedOk ? "Да" : "Нет")}";
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+
+        public static void Wr(object sender, EventArgs args)
+        {
+            Console.WriteLine("Работа началась...");
+            Thread.Sleep(5000);
+            Console.WriteLine("Работа завершилась...");
+        }
+    }
+}
